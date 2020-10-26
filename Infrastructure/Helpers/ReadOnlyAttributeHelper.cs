87d5db3359c9b08fec6d8a80b8d9be7a2b147ecd
fahminlb33 <fahminlb33@gmail.com>
2020-10-26 15:34:51 +0700

    Added more tag helpers

---
+++ Infrastructure/Helpers/ReadOnlyAttributeHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
+
+namespace Venjix.Infrastructure.Helpers
+{
+    [HtmlTargetElement("*", Attributes = "readonly-when")]
+    public class ReadOnlyAttributeHelper : TagHelper
+    {
+        public bool ReadonlyWhen { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContextData { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ReadonlyWhen)
+            {
+                output.Attributes.SetAttribute("readonly", "readonly");
+            }
+            else
+            {
+                if (output.Attributes.ContainsName("readonly"))
+                {
+                    output.Attributes.Remove(output.Attributes.First(x => x.Name == "readonly"));
+                }
+            }
+        }
+
+    }
+}
---
+++ Infrastructure/Helpers/ViewDataKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace Venjix.Infrastructure.Helpers
+{
+    public static class ViewDataKeys
+    {
+        public const string Title = nameof(Title);
+        public const string PageHeading = nameof(PageHeading);
+        public const string PageSubheading = nameof(PageSubheading);
+        public const string Message = nameof(Message);
+        public const string IsSuccess = nameof(IsSuccess);
+    }
+}
