d53c56e6e6e63b0d7f0769198c105f5c32e12fdd
fahminlb33 <fahminlb33@gmail.com>
2020-10-26 15:34:35 +0700

    Remove redundant code

--- Infrastructure/DataTables/OrderingConverter.cs
+++ Infrastructure/DataTables/OrderingConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -9,13 +8,12 @@ namespace Venjix.Infrastructure.DataTables
     {
         public override DataTablesOrdering Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            Debug.Print(reader.GetString());
             return reader.GetString() == "asc" ? DataTablesOrdering.Ascending : DataTablesOrdering.Descending;
         }
 
         public override void Write(Utf8JsonWriter writer, DataTablesOrdering value, JsonSerializerOptions options)
         {
-            if ((DataTablesOrdering)value == DataTablesOrdering.Ascending)
+            if (value == DataTablesOrdering.Ascending)
             {
                 writer.WriteStringValue("asc");
             }
