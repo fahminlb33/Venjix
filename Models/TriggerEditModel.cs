fdd501cc42003729512adcc17be647f5135dadab
fahminlb33 <fahminlb33@gmail.com>
2020-10-27 19:25:36 +0700

    Preliminary trigger layout

---
+++ Models/TriggerEditModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Venjix.Models
+{
+    public class TriggerEditModel
+    {
+    }
+}
