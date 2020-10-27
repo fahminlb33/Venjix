5414ee6050474e71fef660a47fd12050b193e3e3
fahminlb33 <fahminlb33@gmail.com>
2020-10-27 19:40:53 +0700

    Refactor new namespace

---
+++ Models/VisualizeTableModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace Venjix.Models
+{
+    public class VisualizeTableModel
+    {
+        public int SensorId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public List<SelectListItem> Sensors { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Venjix.DAL;
+using Venjix.Infrastructure.DAL;
 using Venjix.Infrastructure.DataTables;
 
 namespace Venjix
