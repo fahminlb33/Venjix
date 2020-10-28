ad0f9b8092616904acb636384f09c188d00cc05a
fahminlb33 <fahminlb33@gmail.com>
2020-10-28 10:58:57 +0700

    Add not null or whitespace validation

---
+++ Infrastructure/Helpers/NotNullOrWhiteSpaceAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Venjix.Infrastructure.Helpers
+{
+    public class NotNullOrWhiteSpaceAttribute : ValidationAttribute
+    {
+        public NotNullOrWhiteSpaceAttribute() : base("Invalid Field") { }
+        public NotNullOrWhiteSpaceAttribute(string Message) : base(Message) { }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!string.IsNullOrWhiteSpace(value?.ToString())) return ValidationResult.Success;
+            return new ValidationResult("Value cannot be empty or white space.");
+        }
+    }
+}
