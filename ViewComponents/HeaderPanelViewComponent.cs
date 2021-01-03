2e78a33bf1c8347df1a3fbb17a89d8b7bc8b5417
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2021-01-03 19:28:23 +0700

    Code style

--- ViewComponents/HeaderPanelViewComponent.cs
+++ ViewComponents/HeaderPanelViewComponent.cs
@@ -1,39 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Venjix.ViewComponents
 {
-   
-
     [ViewComponent(Name = "HeaderPanel")]
     public class HeaderPanelViewComponent : ViewComponent
     {
-        public class Model {
+        public class Model 
+        {
             public string PageSubheading { get; set; }
-
             public string PageHeading { get; set; } = "Heading";
-
             public string Action { get; set; }
-
             public string ActionLabel { get; set; }
-
             public string Controller { get; set; }
             public string ActionIcon { get; set; }
         }
 
-        
-         
-
         public IViewComponentResult Invoke(string pageHeading , string pageSubheading, string action = "", string actionLabel = "", string controller = "", string actionIcon = "")
         {
-            Model m = new() { PageHeading = pageHeading, PageSubheading = pageSubheading, Action = action, ActionLabel = actionLabel, Controller = controller, ActionIcon = actionIcon };
+            Model m = new() {
+                PageHeading = pageHeading,
+                PageSubheading = pageSubheading, 
+                Action = action, 
+                ActionLabel = actionLabel, 
+                Controller = controller,
+                ActionIcon = actionIcon 
+            };
 
             return View("Default", m);
         }
-
     }
 }
