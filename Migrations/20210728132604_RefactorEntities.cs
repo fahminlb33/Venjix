d06457473f0283fc680639e2a6506a2033ea23eb
fahminlb33 <fahminlb33@gmail.com>
2021-07-28 20:26:59 +0700

    Add migration after refactoring

---
+++ Migrations/20210728132604_RefactorEntities.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Venjix.Migrations
+{
+    public partial class RefactorEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
